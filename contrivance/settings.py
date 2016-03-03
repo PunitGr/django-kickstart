2d69ae805487b38bf250a19927eb38cf966fe9dc
PunitGr <groove679@gmail.com>
2016-03-03 19:31:01 +0530

    create models for player and team

--- contrivance/settings.py
+++ contrivance/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'iplauction.app.IplauctionConfig',
+    'iplauction.apps.IplauctionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- iplauction/models.py
+++ iplauction/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+
+class Player(models.Model):
+    player_id = models.IntegerField(primary_key=True)
+    player_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=20)
+    country = models.CharField(max_length=30)
+    base_price = models.FloatField(default=100000)
+    xp = models.IntegerField(default=None)
+    sold = models.BooleanField(default=False)
+    sold_to = models.OneToOneField(Team)
+
+
+class Team(models.Model):
+    team_id = models.IntegerField(primary_key=True)
+    team_name = models.CharField(max_length=20)
+    budget = models.FloatField(default=200000000)
+    player_list = models.ForeignKey(Player)
